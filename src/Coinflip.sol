a27c96e938cbc08297038abeb00b9f2d013785cf
TC <Qwerky7835@users.noreply.github.com>
2025-01-31 17:27:28 +0100

    refresh game rules 2025

--- src/Coinflip.sol
+++ src/Coinflip.sol
@@ -2,45 +2,41 @@
 pragma solidity ^0.8.23;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {VRFv2DirectFundingConsumer} from "./VRFv2DirectFundingConsumer.sol";
+import {DirectFundingConsumer} from "./DirectFundingConsumer.sol";
 
 contract Coinflip is Ownable{
-    // A map of the player and their corresponding random number request
+    // A map of the player and their corresponding requestId
     mapping(address => uint256) public playerRequestID;
-    // A map that stores the users coinflip guess
-    mapping(address => uint8) public bets;
-    // An instance of the random number resquestor, client interface
-    VRFv2DirectFundingConsumer private vrfRequestor;
+    // A map that stores the player's 3 Coinflip guesses
+    mapping(address => uint8[3]) public bets;
+    // An instance of the random number requestor, client interface
+    DirectFundingConsumer private vrfRequestor;
 
-    ///@dev we no longer use the seed, instead each coinflip should spawn its own VRF instance
-    ///@notice This programming pattern is a factory model - a contract creating other contracts 
+    ///@dev we no longer use the seed, instead each coinflip deployment should spawn its own VRF instance so that the Coinflip smart contract is the owner of the DirectFunding contract.
+    ///@notice This programming pattern is known as a factory model - a contract creating other contracts 
     constructor() Ownable(msg.sender) {}
 
-    ///@notice Fund the VRF instance with **2** LINK tokens.
-    ///@return A boolean of whether funding the VRF instance with link tokens was successful or not
+    ///@notice Fund the VRF instance with **5** LINK tokens.
+    ///@return boolean of whether funding the VRF instance with link tokens was successful or not
     ///@dev use the address of LINK token contract provided. Do not change the address!
-    ///@custom:attention In order for this contract to fund another contract, which tokens does it require to have before calling this function?
-    ///                  What **additional** functions does this contract need to receive these tokens itself?
+    ///@custom: Attention! In order for this contract to fund another contract, which tokens does this contract need to have before calling this function? What **additional** functions does this contract need to "receive" these tokens itself?
     function fundOracle() external returns(bool){
         address Link_addr = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
     }
 
-    ///@notice user guess only ONE flip either a 1 or a 0.
-    ///@param a uint8 which is required to be 1 or 0
-    ///@dev After validating the user input, store the user input in global mapping and fire off a request to the VRF instance
+    ///@notice user guess THREE flips either a 1 or a 0.
+    ///@param uint8 3 guesses - which is "required" to be 1 or 0
+    ///@dev After validating the user input, store the user input and request ID in their respective global mappings and call the "requestRandomWords" function in VRF instance
+    ///@custom: Attention! How do we make sure 3 random numbers are requested?
     ///@dev Then, store the requestid in global mapping
-    function userInput(uint8 Guess) external {}
+    function userInput(uint8[3] Guesses) external {}
 
-    ///@notice due to the fact that a blockchain does not deliver data instantaneously, in fact quite slowly under congestion, allow
-    ///        users to check the status of their request.
-    ///@return a boolean of whether the request has been fulfilled or not
+    ///@notice Due to the fact that a blockchain does not deliver data instantaneously, in fact quite slowly under congestion, allow users to check the status of their request.
+    ///@return boolean of whether the request has been fulfilled or not
     function checkStatus() external view returns(bool){}
 
-    ///@notice once the request is fulfilled, return the random result and check if user won
-    ///@return a boolean of whether the user won or not based on their input
-    ///@dev request the randomWord that is returned. Here you need to check the VRFcontract to understand what type the random word is returned in
-    ///@dev simply take the first result, or you can configure the VRF to only return 1 number, and check if it is even or odd. 
-    ///     if it is even, the randomly generated flip is 0 and if it is odd, the random flip is 1
-    ///@dev compare the user guess with the generated flip and return if these two inputs match.
+    ///@return boolean of whether the user won or not based on their input
+    ///@dev Check if whether each of the three random numbers is even or odd. If it is even, the randomly generated flip is 0 and if it is odd, the random flip is 1.
+    ///@notice Player wins if the 1, 0 flips of the contract matches the 3 guesses of the player.
     function determineFlip() external view returns(bool){}
 }
